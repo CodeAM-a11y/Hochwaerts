159a1b0f589912d8ae89692336e39430a75e2005
CodeAM-a11y <mutaf@mailbox.org>
2026-03-04 13:16:03 +0100

    04.03.26 13:15

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -19,11 +19,13 @@ public class HomeController : Controller
         var verkaufbareBücher = _context.Verleihobjekt
             .Include( verleihobjekt=>verleihobjekt.Buch)
             .ThenInclude(buch =>buch.Autoren).Where(y=>y.StatusId==4);
+        ViewBag.Autorenliste = new SelectList(_context.Autor, "Name", "Name");
         return View(await verkaufbareBücher.ToListAsync());
     }
 
     public IActionResult Privacy()
     {
+        ViewBag.Autorenliste = new SelectList(_context.Autor, "Name", "Name");
         return View();
     }
     public async Task<IActionResult> kaufen(int? id)
@@ -44,6 +46,7 @@ public class HomeController : Controller
             return NotFound();
         }
 
+        ViewBag.Autorenliste = new SelectList(_context.Autor, "Name", "Name");
         return View(verleihobjekt);
     }
     [HttpPost, ActionName("kaufen")]
@@ -56,6 +59,7 @@ public class HomeController : Controller
             _context.Verleihobjekt.Remove(verleihobjekt);
         }
 
+        ViewBag.Autorenliste = new SelectList(_context.Autor, "Name", "Name");
         await _context.SaveChangesAsync();
         return RedirectToAction(nameof(Index));
     }
@@ -64,6 +68,7 @@ public class HomeController : Controller
         var verkaufbareBücher = _context.Verleihobjekt
             .Include(x=>x.Buch).ThenInclude(buch =>buch.Autoren ).Where(y=>y.StatusId==4)
             .OrderBy(x=>x.Buch.Titel);
+        ViewBag.Autorenliste = new SelectList(_context.Autor, "Name", "Name");
         return View(nameof(Index),await verkaufbareBücher.ToListAsync());
     }
     public async Task<IActionResult> SortAutor()
@@ -73,6 +78,7 @@ public class HomeController : Controller
             .ThenInclude(buch =>buch.Autoren )
             .Where(y=>y.StatusId==4)
             .OrderBy(x=>x.Buch.Autoren.OrderBy(a=>a.Name).FirstOrDefault().Name);
+        ViewBag.Autorenliste = new SelectList(_context.Autor, "Name", "Name");
         return View(nameof(Index),await verkaufbareBücher.ToListAsync());
     }
     public async Task<IActionResult> SortDate()
@@ -80,7 +86,21 @@ public class HomeController : Controller
         var verkaufbareBücher = _context.Verleihobjekt
             .Include(x=>x.Buch).ThenInclude(buch =>buch.Autoren ).Where(y=>y.StatusId==4)
             .OrderBy(x=>x.Buch.Erscheinungsjahr);
+        ViewBag.Autorenliste = new SelectList(_context.Autor, "Name", "Name");
         return View(nameof(Index),await verkaufbareBücher.ToListAsync());
+    }
+
+    public async Task<IActionResult> FilterAutor(string autor)
+    {
+        var verkaufbareBücher = _context.Verleihobjekt.Include(verleihobjekt =>  verleihobjekt.Buch).
+            ThenInclude(buch => buch.Autoren).Where(verleihobjekt => verleihobjekt.StatusId == 4);
+        if (!string.IsNullOrEmpty(autor)) { 
+            verkaufbareBücher = verkaufbareBücher
+            .Where(v => v.Buch.Autoren.Any(a => a.Name.Equals(autor))); 
+        } 
+        ViewBag.Autorenliste = new SelectList(_context.Autor, "Name", "Name");
+        var result = await verkaufbareBücher.ToListAsync(); 
+        return View(nameof(Index), result);
     }
 
     [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
